b16b59e19a152de9c7fd8e221c5fee9d40b3d150
elliotchun <eschun@uci.edu>
2026-05-30 14:43:48 -0700

    Fix a race condition in pdf to markdown script

--- scripts/process-policy-doc.ts
+++ scripts/process-policy-doc.ts
@@ -17,12 +17,14 @@ if (process.argv[1] === import.meta.filename) { // From command line
         console.error("Unsupported file extension.");
         process.exit(1);
     }
-    await convertPdfToMarkdown(path, "result.md");
+
+    const result = path.slice(0, -4);
+    await convertPdfToMarkdown(path, `${result}.md`);
     console.log('Done.');
 }
 
 export async function convertPdfToMarkdown(inputFile: string, outputFile: string) {
     const pdfBuffer = await Bun.file(inputFile).arrayBuffer();
     const text = await pdf2md(pdfBuffer);
-    Bun.file(outputFile).write(text);
+    await Bun.file(outputFile).write(text);
 }
