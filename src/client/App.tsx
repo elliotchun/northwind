d8e593cd61d7cb81e5c2ccf22b78bdbef48fb2b2
elliotchun <eschun@uci.edu>
2026-05-29 19:34:02 -0700

    Add React dependency

---
+++ src/client/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+export function App() {
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import staticPlugin from "@elysia/static";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(staticPlugin({
+    assets: 'src/client/',
+    bunFullstack: true,
+    prefix: '/',
+  }))
+  .listen(3000);
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
